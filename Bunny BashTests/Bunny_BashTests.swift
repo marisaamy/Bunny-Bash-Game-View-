0bbeed5834c69c129c94424e0e61412ef3cc85a2
Marisa Wadsworth <marisawadsworth@marisas-air.hub>
2018-03-08 19:07:02 +1300

    Initial Commit

---
+++ Bunny BashTests/Bunny_BashTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bunny_BashTests.swift
+//  Bunny BashTests
+//
+//  Created by Marisa Wadsworth on 8/03/18.
+//  Copyright © 2018 Marisa Wadsworth. All rights reserved.
+//
+
+import XCTest
+@testable import Bunny_Bash
+
+class Bunny_BashTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
